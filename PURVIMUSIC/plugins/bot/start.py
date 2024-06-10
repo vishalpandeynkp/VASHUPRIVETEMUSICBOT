78e3d3409ade64207782594e46ddaef016d13e06
masoombalak88 <vikashyadav735556@gmail.com>
2024-06-10 11:28:13 +0530

    Update start.py

--- PURVIMUSIC/plugins/bot/start.py
+++ PURVIMUSIC/plugins/bot/start.py
@@ -101,7 +101,7 @@ async def start_pm(client, message: Message, _):
                 )
     else:
         out = private_panel(_)
-        await message.reply_video(
+        await message.reply_photo(
             random.choice(NEXI_VID),
             caption=_["start_2"].format(message.from_user.mention, app.mention),
             reply_markup=InlineKeyboardMarkup(out),
@@ -118,7 +118,7 @@ async def start_pm(client, message: Message, _):
 async def start_gp(client, message: Message, _):
     out = start_panel(_)
     uptime = int(time.time() - _boot_)
-    await message.reply_video(
+    await message.reply_photo(
         random.choice(NEXI_VID),
         caption=_["start_1"].format(app.mention, get_readable_time(uptime)),
         reply_markup=InlineKeyboardMarkup(out),
@@ -153,7 +153,7 @@ async def welcome(client, message: Message):
                     return await app.leave_chat(message.chat.id)
 
                 out = start_panel(_)
-                await message.reply_video(
+                await message.reply_photo(
                     random.choice(NEXI_VID),
                     caption=_["start_3"].format(
                         message.from_user.mention,
