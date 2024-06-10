c06162c508e7505bd80342a1be1161b124321cfd
masoombalak88 <vikashyadav735556@gmail.com>
2024-06-10 09:56:36 +0530

    Update ping.py

--- PURVIMUSIC/plugins/tools/ping.py
+++ PURVIMUSIC/plugins/tools/ping.py
@@ -17,7 +17,7 @@ from config import BANNED_USERS
 async def ping_com(client, message: Message, _):
     start = datetime.now()
     response = await message.reply_video(
-        video="https://graph.org/file/5690109178f081adf464d.mp4",
+        photo="https://telegra.ph/file/c8183c9f6573f7eb799d2.jpg",
         caption=_["ping_1"].format(app.mention),
     )
     pytgping = await PURVI.ping()
