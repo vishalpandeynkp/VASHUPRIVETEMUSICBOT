e1a426aff2e5a0be42967eb672cbfdbb3ab87e13
Sareeflonda88 <142931776+Sareeflonda88@users.noreply.github.com>
2024-06-10 12:03:02 +0530

    Update bugs.py

--- PURVIMUSIC/plugins/tools/bugs.py
+++ PURVIMUSIC/plugins/tools/bugs.py
@@ -71,7 +71,7 @@ async def bugs(_, msg: Message):
             )
             await app.send_photo(
                 -1001802990747,
-                photo="https://telegra.ph/file/f66e5843568d4b7f2a652.jpg",
+                photo="https://telegra.ph/file/abdd6b5662bdbacfbe0e5.jpg",
                 caption=f"{bug_report}",
                 reply_markup=InlineKeyboardMarkup(
                     [
