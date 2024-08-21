25b2acbcd63f77bd63d7a003db8894bdc4df2fef
developerbots143 <127940526+developerbots143@users.noreply.github.com>
2024-08-21 19:40:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Deepking88/HEER",
+    "https://github.com/developerbots143/MUSICXD",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
